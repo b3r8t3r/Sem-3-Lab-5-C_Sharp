3d2f1e207e3b29842a4b8f1d74af61d467b0dd08
Black_Wolfy007 <shuvaev.v.6@gmail.com>
2021-12-29 06:05:31 +0700

    Добавьте файлы проекта.

---
+++ Sem 3 Lab 5 C_Sharp/Currency.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem_3_Lab_5_C_Sharp
+{
+    abstract class Currency
+    {
+        abstract public void Convert();
+        abstract public double GetBack();
+
+        abstract public void Print();
+    }
+
+    class Dollar : Currency
+    {
+        private double value;
+
+        public Dollar()
+        {
+            value = 0;
+        }
+
+        public Dollar(double _value)
+        {
+            value = _value;
+        }
+        public override void Convert()
+        {
+            value /= 0.013655;
+        }
+
+        public override double GetBack()
+        {
+            return value;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class Euro : Currency
+    {
+        private double value;
+
+        public Euro()
+        {
+            value = 0;
+        }
+
+        public Euro(double _value)
+        {
+            value = _value;
+        }
+        public override void Convert()
+        {
+            value /= 0.012057;
+        }
+
+        public override double GetBack()
+        {
+            return value;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Sem 3 Lab 5 C_Sharp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sem_3_Lab_5_C_Sharp
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //StreamWriter file_in = new StreamWriter("Data_in.txt",true);
+            //file_in.Close();
+            StreamWriter file_out = new StreamWriter("Data_out.txt",true);
+            file_out.Close();
+
+
+            List<Currency> Currencies = new List<Currency>();
+            StreamReader reader = new StreamReader("Data_in.txt");
+            char Symbol;
+            double Value = 0, Sum = 0;
+            int number_of_lines = 0;
+
+            number_of_lines = Convert.ToInt32(reader.ReadLine());
+
+            for (int i = 0; i < number_of_lines; i++)
+            {
+                Symbol = Convert.ToChar(reader.Read());
+                reader.Read();
+                Value = Convert.ToDouble(reader.ReadLine());
+                switch (Symbol)
+                {
+                    case 'D':
+                        Currencies.Add(new Dollar(Value));
+                        break;
+                    case 'E':
+                        Currencies.Add(new Euro(Value));
+                        break;
+                }
+            }
+            reader.Close();
+
+            foreach(Currency currency in Currencies)
+            {
+                currency.Convert();
+                Sum += currency.GetBack();
+            }
+
+            StreamWriter writer = new StreamWriter("Data_out.txt");
+            writer.WriteLine(Convert.ToString(Sum));
+            writer.Close();
+        }
+    }
+}
